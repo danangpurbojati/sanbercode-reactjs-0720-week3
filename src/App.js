6a688617ef45a6a1fa4c2eb32279ebfa9b829861
danangpurbojati <danangpurbojati@gmail.com>
2020-08-07 05:25:33 +0700

    tugas-14

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import HargaBuah from './tugas11/HargaBuah'
+// import DaftarHargaBuah from './tugas13/DaftarHargaBuah'
+import DaftarHargaBuah from './tugas14/DaftarHargaBuah'
 import Timer from './tugas12/Timer'
-import DaftarHargaBuah from './tugas13/DaftarHargaBuah'
 
 function App() {
   return (
